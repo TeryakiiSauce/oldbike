6d4f7428c7abaf583b2c6e98f31231e7f5066552
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-10-29 21:22:12 +0300

    added basic login screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 // Text Styles
+// =====================
 
 const TextStyle kLargeLabelTS = TextStyle(
   fontSize: 42.0,
-  fontWeight: FontWeight.normal,
+);
+
+const TextStyle kTitleTS = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle kAttentionLabelTS = TextStyle(
+  color: Color(0xffd4d2a5),
 );
--- lib/screens/login-signup/login.dart
+++ lib/screens/login-signup/login.dart
@@ -8,29 +8,76 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-            Expanded(
-              child: Text(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Sign In',
+                style: kTitleTS,
+              ),
+              const Text(
                 'The app\nfor tracking all\nyour bike rides',
                 style: kLargeLabelTS,
               ),
-            ),
-            Expanded(
-              child: Text('Email'),
-            ),
-            Expanded(
-              child: Text('Password'),
-            ),
-            Expanded(
-              child: Icon(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.person),
+                      hintText: 'Username',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: const Icon(Icons.visibility_outlined),
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  const Text(
+                    'Forgot Password?',
+                    textAlign: TextAlign.right,
+                    style: kAttentionLabelTS,
+                  ),
+                ],
+              ),
+              const Icon(
                 Icons.arrow_circle_right_rounded,
                 size: 100.0,
               ),
-            ),
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('Don\'t have an account?'),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  Text(
+                    'Create Account',
+                    style: kAttentionLabelTS,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
