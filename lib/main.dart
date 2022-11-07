6d142b2a9e71ad18fede99fd9fe6f765bbccb087
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-11-07 12:42:37 +0300

    removed the debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class OldBikeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Old Bike',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color(0xFF48435C),
