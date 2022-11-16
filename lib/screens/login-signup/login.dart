d1ee25af6b761f74bee0c40d2a977eae439e5b00
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-11-16 06:03:26 +0300

    I have managed to fix the login screen pixel overflow issue when the keyboard is displayed

--- lib/screens/login-signup/login.dart
+++ lib/screens/login-signup/login.dart
@@ -14,104 +14,116 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    const SizedBox spacing = SizedBox(
+      height: 30.0,
+    );
+
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                'Sign In',
-                style: ktsMainTitle,
-              ),
-              const Text(
-                'The app\nfor tracking all\nyour bike rides',
-                style: ktsNormalLargeLabel,
-              ),
-              Column(
+        child: Center(
+          child: SingleChildScrollView(
+            reverse: true,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.person),
-                      hintText: 'Username',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.lock),
-                      suffixIcon: const Icon(Icons.visibility_outlined),
-                      hintText: 'Password',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15.0,
-                  ),
                   const Text(
-                    'Forgot Password?',
-                    textAlign: TextAlign.right,
-                    style: ktsAttentionLabel,
+                    'Sign In',
+                    style: ktsMainTitle,
                   ),
-                ],
-              ),
-              const Icon(
-                Icons.arrow_circle_right_rounded,
-                size: 100.0,
-              ),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text('Don\'t have an account?'),
-                      SizedBox(
-                        width: 5.0,
+                  spacing,
+                  const Text(
+                    'The app\nfor tracking all\nyour bike rides',
+                    style: ktsNormalLargeLabel,
+                  ),
+                  spacing,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.person),
+                          hintText: 'Username',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
                       ),
-                      Text(
-                        'Create Account',
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.lock),
+                          suffixIcon: const Icon(Icons.visibility_outlined),
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      const Text(
+                        'Forgot Password?',
+                        textAlign: TextAlign.right,
                         style: ktsAttentionLabel,
                       ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 15.0,
-                    width: 5.0,
+                  spacing,
+                  const Icon(
+                    Icons.arrow_circle_right_rounded,
+                    size: 100.0,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  spacing,
+                  Column(
                     children: [
-                      const Text('Wanna do it up later?'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text('Don\'t have an account?'),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            'Create Account',
+                            style: ktsAttentionLabel,
+                          ),
+                        ],
+                      ),
                       const SizedBox(
+                        height: 15.0,
                         width: 5.0,
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushReplacementNamed(
-                              context, HomeScreen.screen);
-                        },
-                        child: const Text(
-                          'Skip',
-                          style: ktsAttentionLabel,
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Wanna do it up later?'),
+                          const SizedBox(
+                            width: 5.0,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacementNamed(
+                                  context, HomeScreen.screen);
+                            },
+                            child: const Text(
+                              'Skip',
+                              style: ktsAttentionLabel,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
