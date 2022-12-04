5582accac0423a8f884695d85fd7ce9a062e058d
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-12-05 02:10:12 +0300

    integrated firebase but now i cant run the simulator on my mac...

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 /// This file includes the theme data as well as the screen routes information.
 /// === === === === ===
 
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:oldbike/screens/home/home.dart';
 import 'package:oldbike/screens/login-signup/login.dart';
@@ -9,7 +11,11 @@ import 'package:oldbike/screens/profile/profile.dart';
 import 'package:oldbike/tab_view_controller.dart';
 import 'package:oldbike/utils/colors.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const OldBikeApp());
 }
 
