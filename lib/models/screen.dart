353331b364367fd8b8c611ed06232a83d094312a
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2022-12-09 06:43:51 +0300

    [cleanup] minor cleanup

--- lib/models/screen.dart
+++ lib/models/screen.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:oldbike/screens/home/home.dart';
-import 'package:oldbike/screens/login-signup/login.dart';
 import 'package:oldbike/screens/profile/profile.dart';
-import 'package:oldbike/screens/tracking/statistics.dart';
 import 'package:oldbike/screens/tracking/track_ride.dart';
 import 'package:oldbike/utils/colors.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
@@ -62,76 +57,4 @@ class Screen {
       ],
     );
   }
-
-  // final Map<TabScreen, Map<String, dynamic>> _screenDetailsList = {
-  //   TabScreen.home: {
-  //     'screenWidget': const HomeScreen(),
-  //     'title': 'Home',
-  //     'actions': [],
-  //   },
-  //   TabScreen.track: {
-  //     'screenWidget': const RideTrackingScreen(),
-  //     'title': 'Track',
-  //     'actions': [],
-  //   },
-  //   TabScreen.profile: {
-  //     'screenWidget': const ProfileScreen(),
-  //     'title': 'Profile',
-  //     'actions': [],
-  //   },
-  //   TabScreen.statistics: {
-  //     'screenWidget': const StatisticsScreen(),
-  //     'title': 'Statistics',
-  //     'actions': [],
-  //   },
-  // };
-
-  // Future<void> fireHapticFeedback() async =>
-  //     await HapticFeedback.selectionClick();
-
-  // List<BottomNavigationBarItem> buildBottomNavBar(
-  //     Map<TabScreen, List<dynamic>> tabScreens) {
-  //   List<BottomNavigationBarItem> list = [];
-  //   for (List<dynamic> currentScreenDetails in tabScreens.values) {
-  //     list.add(
-  //       BottomNavigationBarItem(
-  //         icon: currentScreenDetails[0],
-  //         label: currentScreenDetails[1],
-  //         tooltip: '',
-  //       ),
-  //     );
-  //   }
-  //   return list;
-  // }
-//
-  // Text getScreenTitle({required TabScreen screen}) => Text(
-  //       _screenDetailsList[screen]!['title'],
-  //     );
-//
-  // Widget getScreenWidget({required TabScreen screen}) =>
-  //     _screenDetailsList[screen]!['screenWidget'];
-//
-  // List<Widget> getActions({required TabScreen screen}) {
-  //   switch (screen) {
-  //     case TabScreen.profile:
-  //       _screenDetailsList[screen]!['actions'] = [
-  //         IconButton(
-  //           onPressed: () {
-  //             fireHapticFeedback();
-
-  //             Navigator.pushReplacementNamed(
-  //               context,
-  //               LoginScreen.screen,
-  //             );
-  //           },
-  //           icon: const Icon(Icons.exit_to_app_rounded),
-  //         ),
-  //       ];
-  //       break;
-  //     default:
-  //       _screenDetailsList[screen]!['actions'] = [Container()];
-  //   }
-
-  //   return _screenDetailsList[screen]!['actions'];
-  // }
 }
--- lib/tab_view_controller.dart
+++ lib/tab_view_controller.dart
@@ -5,11 +5,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:oldbike/screens/home/home.dart';
-import 'package:oldbike/screens/profile/profile.dart';
-import 'package:oldbike/screens/tracking/track_ride.dart';
 import 'package:oldbike/utils/colors.dart';
-import 'package:oldbike/utils/platform_based_widgets.dart';
 import 'package:oldbike/models/screen.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
@@ -25,23 +21,6 @@ class TabViewController extends StatefulWidget {
 class _TabViewControllerState extends State<TabViewController> {
   late Screen screen;
   PersistentTabController controller = PersistentTabController(initialIndex: 0);
-
-  // List<BottomNavigationBarItem> getNavBarItems() => screen.buildBottomNavBar(
-  //       {
-  //         TabScreen.home: [
-  //           const Icon(Icons.home_rounded),
-  //           'Home',
-  //         ],
-  //         TabScreen.track: [
-  //           const Icon(Icons.play_arrow_rounded),
-  //           'Track',
-  //         ],
-  //         TabScreen.profile: [
-  //           const Icon(Icons.person),
-  //           'Profile',
-  //         ],
-  //       },
-  //     );
 
   @override
   Widget build(BuildContext context) {
